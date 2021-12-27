25bc9c6d81412d06b809ab7f279fefe8c11fef8c
GangadharDV30 <96405700+GangadharDV30@users.noreply.github.com>
2021-12-27 08:27:50 +0530

    Add files via upload

---
+++ JavaException/src/com/rocky/program/AgeCustExcep.java
@@ -0,0 +1,13 @@
+package com.rocky.program;
+
+
+	public class AgeCustExcep extends Exception{
+		
+
+		//Checked Exception
+				public AgeCustExcep(String message) {
+				super(message);
+			
+		}
+	}
+
---
+++ JavaException/src/com/rocky/program/StoringName.java
@@ -0,0 +1,7 @@
+package com.rocky.program;
+
+public class StoringName {
+	
+		String[] arr=new String[]{"kiran","Gangadhar","Girish","Mahanthesh","Madhu"};
+	}
+
